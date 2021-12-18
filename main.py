c631433df4ce1d74bfe412a6fda116f06019af1d
Guiners <50328216+Guiners@users.noreply.github.com>
2021-12-18 21:48:17 +0100

    Final_Update
    code was formatted

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ clock = pygame.time.Clock()
 
 # lists
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
-color = ["Trefl", "Karo", "Kier", "Pik"]
+colors = ["Trefl", "Karo", "Kier", "Pik"]
 deck = []
 card = []
 player1 = []
@@ -26,13 +26,11 @@ score1 = font.render(("Cards in deck: " + str(len(player1))), True, (41, 58, 162
 score2 = font.render(("Cards in deck: " + str(len(player2))), True, (162, 41, 50))
 
 
-
-
 # making talia
-for k in color:
-    for j in ranks:
-        card.append(j)
-        card.append(k)
+for color in colors:
+    for rank in ranks:
+        card.append(rank)
+        card.append(color)
         deck.append(card)
         card = []
 
@@ -41,15 +39,15 @@ random.shuffle(deck)
 
 # deal the cards
 while len(deck) != 0:
-    k = len(deck)
-    if k % 2 == 0:
+    color = len(deck)
+    if color % 2 == 0:
         player1.append(deck[0])
     else:
         player2.append(deck[0])
     deck.pop(0)
 
 run = True
-font = pygame.font.SysFont('comicsans', 30, True)
+font = pygame.font.SysFont("comicsans", 30, True)
 
 while run:
     clock.tick(27)
@@ -65,10 +63,9 @@ while run:
             sys.exit(0)
 
     screen.fill((255, 255, 255))
-    screen.blit(text1,(10, 1))
-    screen.blit(text2, (1430 - text2.get_width(),1))
+    screen.blit(text1, (10, 1))
+    screen.blit(text2, (1430 - text2.get_width(), 1))
     screen.blit(score1, (10, 800))
     screen.blit(score2, (1430 - score2.get_width(), 800))
 
     pygame.display.flip()
-
