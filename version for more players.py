77d3a7f9004de60b6cf26bf68ceb23a2bcf3235e
Guiners <50328216+Guiners@users.noreply.github.com>
2021-12-18 21:49:09 +0100

    Final_Update
    changed var names
    code formatted

--- version for more players.py
+++ version for more players.py
@@ -10,102 +10,96 @@ while True:
 
 def deck_making():
     ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
-    color = ["Trefl", "Karo", "Kier", "Pik"]
+    colors = ["Trefl", "Karo", "Kier", "Pik"]
     deck = []
     card = []
     number_of_decks = int(np.ceil(number_of_players / 6))
-    for i in range(number_of_decks):
-        for k in color:
-            for j in ranks:
-                card.append(j)
-                card.append(k+str(i))
+    for iteration in range(number_of_decks):
+        for color in colors:
+            for rank in ranks:
+                card.append(rank)
+                card.append(color + str(iteration))
                 deck.append(card)
                 card = []
     random.shuffle(deck)
     return deck
 
 
-
-
 def dealing(deck, number_of_players):
     list_of_players = []
-    for i in range(number_of_players):
+    for number in range(number_of_players):
         list_of_players.append([])
 
     while len(deck) != 0:
-        for i in range(number_of_players):
+        for number in range(number_of_players):
             if len(deck) != 0:
-                list_of_players[i].append(deck[0])
+                list_of_players[number].append(deck[0])
                 deck.pop(0)
             else:
                 break
     return list_of_players
 
 
-
-
-def fight(list_of_players, extra_stos = [], winers = [],):
+def fight(
+    list_of_players,
+    extra_stos=[],
+    winners2=[],
+):
     war = []
     stos = []
-    winers2 = []
-    #licznik = 0
+    winners = []
     in_game = []
-    for j in range(len(list_of_players)):
-        #licznik += len(list_of_players[j])
-        #print('a', list_of_players[j])
-        if len(list_of_players[j]) == 0:
-            winers.append(str("Player" + str(j)))
+    for number in range(len(list_of_players)):
+        if len(list_of_players[number]) == 0:
+            winners2.append(str("Player" + str(number)))
             pass
 
         else:
-            #print("player", j, "has", len(list_of_players[j]))
-            in_game.append(j)
-            #print("players in game",in_game)
-            stos.append(list_of_players[j][0])
-            print('player', j, "put", list_of_players[j][0])
-            list_of_players[j].pop(0)
-    #print("liczba kart", licznik)
+            in_game.append(number)
+            stos.append(list_of_players[number][0])
+            print("player", number, "put", list_of_players[number][0])
+            list_of_players[number].pop(0)
     copy_stos = stos.copy()
     copy_stos.sort()
     if len(copy_stos) > 1:
         if copy_stos[0][0] != copy_stos[1][0]:
             war.append(stos.index(copy_stos[0]))
-            print('player numer', war, 'lost')
+            print("player numer", war, "lost")
             stos.extend(extra_stos)
             list_of_players[in_game[war[0]]].extend(stos)
             stos = []
 
         else:
             war.append(stos.index(copy_stos[0]))
-            for k in range(len(stos)):
-                if k != 0:
-                    if copy_stos[0][0] == copy_stos[k][0]:
-                        index2 = stos.index(copy_stos[k])
+            for card_number in range(len(stos)):
+                if card_number != 0:
+                    if copy_stos[0][0] == copy_stos[card_number][0]:
+                        index2 = stos.index(copy_stos[card_number])
                         war.append(index2)
                         war_copy = war.copy()
-                        for i in range(len(war_copy)):
-                            war[i] = list_of_players[i]
-            fight(war,stos)
+                        for iteration in range(len(war_copy)):
+                            war[iteration] = list_of_players[iteration]
+            fight(war, stos)
 
     else:
         stos.extend(extra_stos)
         list_of_players[in_game[0]].extend(stos)
         stos = []
 
-    for w in winers:
-        if w not in winers2:
-            winers2.append(w)
+    for winner in winners2:
+        if winner not in winners:
+            winners.append(winner)
 
-    return winers2
+    return winners
 
 
 card_deck = deck_making()
 players = dealing(card_deck, number_of_players)
-#print(type(number_of_players))
+
 while True:
-    winers3 = fight(players)
-    if len(winers3) == number_of_players:
+    winners2 = fight(players)
+    if len(winners2) == number_of_players:
         break
     time.sleep(1)
 
-print("Winers in sequence", winers3)
+print("Winers in sequence", winners2)
